f65f83a67aa708a5c9151b4903716549e429e41e
커피볶는정콩 <61773021+DevelopJKong@users.noreply.github.com>
2022-07-27 12:15:56 +0900

    수정 완료

--- frontend/src/redux/userRedux.js
+++ frontend/src/redux/userRedux.js
@@ -1,29 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-  name: "user",
-  initialState: {
-    currentUser: null,
-    isFetching: false,
-    error: false,
-  },
-  reducers: {
-    loginStart: (state) => {
-      state.isFetching = true;
+    name: "user",
+    initialState: {
+        currentUser: null,
+        isFetching: false,
+        error: false,
     },
-    loginSuccess: (state, action) => {
-      state.isFetching = false;
-      state.currentUser = action.payload;
+    reducers: {
+        loginStart: (state) => {
+            state.isFetching = true;
+        },
+        loginSuccess: (state, action) => {
+            state.isFetching = false;
+            state.currentUser = action.payload;
+        },
+        loginFailure: (state) => {
+            state.isFetching = false;
+            state.error = true;
+        },
+        logOut: (state) => {
+            state.isFetching = false;
+            state.currentUser = null;
+            state.error = false;
+        },
     },
-    loginFailure: (state) => {
-      state.isFetching = false;
-      state.error = true;
-    },
-    logOut: () => {
-        localStorage.clear();
-    }
-  },
 });
 
-export const { loginStart, loginSuccess, loginFailure,logOut } = userSlice.actions;
+export const { loginStart, loginSuccess, loginFailure, logOut } = userSlice.actions;
 export default userSlice.reducer;
