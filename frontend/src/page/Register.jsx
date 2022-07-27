36b4226a6fd6bf5c43b429cb82d8ae3c486fb736
커피볶는정콩 <61773021+DevelopJKong@users.noreply.github.com>
2022-07-27 12:11:51 +0900

    register 구현중

---
+++ frontend/src/page/Register.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Form = styled.form``;
+const Input = styled.input``;
+const Button = styled.button``;
+const Error = styled.div``;
+
+
+
+const Register = () => {
+  return (
+    <Form>
+        <Input/>
+        <Input/>
+        <Input/>
+        <Input/>
+        <Button></Button>
+        <Error></Error>
+    </Form>
+  )
+}
+
+export default Register
